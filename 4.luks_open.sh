a2951214489ecefb418703f88e7a034d85217490
hvisage <hvisage@users.noreply.github.com>
2020-09-07 09:26:33 +0200

    Update 4.luks_open.sh

--- 4.luks_open.sh
+++ 4.luks_open.sh
@@ -4,5 +4,5 @@
 #mount /dev/mmcblk0p1 /tmp/boot/
 #/tmp/boot/install/4.luks_open.sh
 
-cryptsetup luksOpen /dev/mmcblk0p2 sdcard
+cryptsetup --keyfile=/.test --allow-discard luksOpen /dev/mmcblk0p2 sdcard 
 exit
